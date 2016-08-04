88ea76f967168920c29befb676e9ea0bba154d3a
Sam (CosmicBagel) Whiteley <sam@cosmicbagel.com>
2016-08-04 00:25:55 -0600

    added typings

--- gl.js
+++ gl.js
@@ -1,4 +1,3 @@
-/* exported webGlStart() */
 (function () {
 	"use strict";
 
@@ -75,8 +74,8 @@
 			"uvMatrix");
 	}
 
-	var mvMatrix = mat4.create();
-	var pMatrix = mat4.create();
+	var mvMatrix = vec3.create();
+	var pMatrix = vec3.create();
 
 	function setMatrixUniforms() {
 		gl.uniformMatrix4fv(shaderProgram.pMatrixUniform, false, pMatrix);
@@ -114,7 +113,7 @@
 	function drawScene() {
 		gl.viewport(-8, -8, gl.viewportWidth, gl.viewportHeight);
 		gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-		
+				
 		mat4.perspective(45, gl.viewportWidth / gl.viewportHeight, 0.1, 100.0, pMatrix);
 		mat4.identity(mvMatrix);
 		mat4.translate(mvMatrix, [-1.5, 0.0, -7.0]);
